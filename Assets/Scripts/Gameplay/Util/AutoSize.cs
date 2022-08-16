673bb596b847bf4c7e3e3dfaf386b1121427ae07
kremnev8 <kremnev8super@yandex.ru>
2022-08-16 18:58:53 +0300

    Visual improvements to water

---
+++ Assets/Scripts/Gameplay/Util/AutoSize.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
+using Util;
+
+namespace Gameplay.Util
+{
+    [ExecuteInEditMode]
+    public class AutoSize : MonoBehaviour
+    {
+        public Tilemap masterTilemap;
+        public SpriteRenderer waterRenderer;
+
+        public SpriteRenderer darkTopRenderer;
+        public SpriteRenderer darkRightRenderer;
+        public SpriteRenderer darkBottomRenderer;
+        public SpriteRenderer darkLeftRenderer;
+
+        public float darkThinkness;
+        
+        
+        private void Awake()
+        {
+            if (Application.isPlaying)
+            {
+                Destroy(this);
+            }
+        }
+
+        private void Update()
+        {
+            if (masterTilemap != null)
+            {
+                masterTilemap.CompressBounds();
+
+                Bounds bounds = masterTilemap.localBounds;
+                
+                Vector2 worldMin = masterTilemap.transform.TransformPoint(bounds.min); 
+                Vector2 worldMax = masterTilemap.transform.TransformPoint(bounds.max);
+
+                
+                Vector2 center = (worldMin + worldMax) / 2f;
+                Vector2 size = worldMax - worldMin;
+
+                waterRenderer.size = size;
+                waterRenderer.transform.localPosition = center;
+
+                Vector2 darkOffset = size / 2f + new Vector2(darkThinkness / 2f - 1, darkThinkness / 2f - 1);
+                Vector2 darkArea = size + new Vector2(darkThinkness * 2f - 2, darkThinkness * 2f - 2);
+                
+                Vector2 darkPos = center + new Vector2(0, darkOffset.y);
+                Vector2 darkSize = new Vector2(darkArea.x, darkThinkness);
+                darkTopRenderer.size = darkSize;
+                darkTopRenderer.transform.localPosition = darkPos;
+
+                darkPos = center + new Vector2(darkOffset.x, 0);
+                darkSize = new Vector2(darkThinkness, darkArea.y);
+                darkRightRenderer.size = darkSize;
+                darkRightRenderer.transform.localPosition = darkPos;
+                
+                darkPos = center + new Vector2(0, -darkOffset.y + 1);
+                darkSize = new Vector2(darkArea.x, darkThinkness + 2);
+                darkBottomRenderer.size = darkSize;
+                darkBottomRenderer.transform.localPosition = darkPos;
+                
+                darkPos = center + new Vector2(-darkOffset.x, 0);
+                darkSize = new Vector2(darkThinkness, darkArea.y);
+                darkLeftRenderer.size = darkSize;
+                darkLeftRenderer.transform.localPosition = darkPos;
+            }
+        }
+    }
+}
--- Assets/Scripts/Gameplay/World/TileMaps/BigGridRuleTile.cs
+++ Assets/Scripts/Gameplay/World/TileMaps/BigGridRuleTile.cs
@@ -1,41 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName = "New Big Grid Tile", menuName = "2D/Tiles/Big Grid Tile", order = 0)]
-public class BigGridRuleTile : TileBase
+namespace TileMaps
 {
-    public Vector3Int offset;
-    public Sprite[] sprites;
-    
-    /// <summary>
-    /// Retrieves any tile rendering data from the scripted tile.
-    /// </summary>
-    /// <param name="position">Position of the Tile on the Tilemap.</param>
-    /// <param name="tilemap">The Tilemap the tile is present on.</param>
-    /// <param name="tileData">Data to render the tile.</param>
-    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    [CreateAssetMenu(fileName = "New Big Grid Rule Tile", menuName = "2D/Tiles/Big Grid Rule Tile", order = 0)]
+    public class BigGridRuleTile : RuleTile
     {
-        var iden = Matrix4x4.identity;
+        public Vector3Int offset;
 
-        Vector3Int pos = position + offset;
-
-        int xoff = Mathf.Abs(pos.x % 2);
-        int yoff = Mathf.Abs(pos.y % 2) * 2;
-        int index = xoff + yoff;
-        if (sprites != null && index < sprites.Length)
+        /// <summary>
+        /// Retrieves any tile rendering data from the scripted tile.
+        /// </summary>
+        /// <param name="position">Position of the Tile on the Tilemap.</param>
+        /// <param name="tilemap">The Tilemap the tile is present on.</param>
+        /// <param name="tileData">Data to render the tile.</param>
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
         {
-            tileData.sprite = sprites[index];
-        }
-        else
-        {
-            tileData.sprite = default;
-        }
+            var iden = Matrix4x4.identity;
+            iden = Matrix4x4.identity;
 
-        tileData.gameObject = default;
-        tileData.colliderType = Tile.ColliderType.None;
-        tileData.flags = TileFlags.LockTransform;
-        tileData.transform = iden;
+            tileData.sprite = m_DefaultSprite;
+            tileData.gameObject = m_DefaultGameObject;
+            tileData.colliderType = m_DefaultColliderType;
+            tileData.flags = TileFlags.LockTransform;
+            tileData.transform = iden;
+
+            Matrix4x4 transform = iden;
+            foreach (TilingRule rule in m_TilingRules)
+            {
+                if (RuleMatches(rule, position, tilemap, ref transform))
+                {
+                    Vector3Int pos = position + offset;
+
+                    int xoff = Mathf.Abs(pos.x % 2);
+                    int yoff = Mathf.Abs(pos.y % 2) * 2;
+                    int index = xoff + yoff;
+                    if (rule.m_Sprites != null && index < rule.m_Sprites.Length)
+                    {
+                        tileData.sprite = rule.m_Sprites[index];
+                    }
+                    else
+                    {
+                        tileData.sprite = rule.m_Sprites[0];
+                    }
+
+                    tileData.gameObject = rule.m_GameObject;
+                    tileData.colliderType = rule.m_ColliderType;
+                    break;
+                }
+            }
+        }
     }
 }
